c6ebf55802277be700737bc369aa5e2ae7a3dd28
OF4621 <xmd19970616@gmail.com>
2022-09-03 17:31:56 +0800

    chore: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App text-3xl">
-      123123
+
+    <div>
+      <div>
+        asd
+      </div>
+
+      <div className="App text-3xl">
+        123123
+      </div>
     </div>
+
   )
 }
 
