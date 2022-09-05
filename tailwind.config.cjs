4c2ee41540f16f23ad1b5e53343516e150b69a21
OF4621 <xmd19970616@gmail.com>
2022-09-05 17:34:46 +0800

    build: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// https://tailwindcss.com/docs/configuration#core-plugins
+
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
 }
